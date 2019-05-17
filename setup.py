be6a2dd0b94030234249babbeb4e33952381f43a
Marcelo Tambalo <marcelo.tambalo@nectosystems.com.br>
2019-05-17 19:10:39 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='xpath_validator',
     packages=['xpath_validator'],
-    version='1.0.3',
+    version='1.0.5',
     description='validate boolean expressions with XPath syntax',
     long_description=open('README.rst').read(),
     author='Marcelo Fonseca Tambalo',
